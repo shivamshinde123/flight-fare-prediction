2b7794f9d855d03c6d668332dadfe9635549f96d
Shivam Dattatray Shinde <54674972+shivamshinde123@users.noreply.github.com>
2022-03-06 01:17:00 +0530

    Minor changes made

---
+++ Training_data_validation_and_db_insertion/trainingValidationAndDBInsertion.py
@@ -0,0 +1,6 @@
+from Logging import logging
+from Training_raw_data_validation.rawDataValidation import rawDataValidation
+from Raw_Data_Transformation.RawDataTransformation import RawTrainingDataTransformation
+from DB_Operations.dataInsertionIntoDB import DBOperations
+
+
