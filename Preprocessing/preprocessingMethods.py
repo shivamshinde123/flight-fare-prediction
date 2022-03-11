f65e19dd4950c93b5463cecc1fd569d9297a82e5
Shivam Shinde <shivamshinde92722@gmail.com>
2022-03-12 02:44:19 +0530

    Added logging for the preprocessing methods

--- Preprocessing/preprocessingMethods.py
+++ Preprocessing/preprocessingMethods.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pandas as pd
 import re
+from Logging.logging import Logger
 
 
 class PreprocessingMethods:
@@ -18,6 +19,9 @@ class PreprocessingMethods:
 
     def __int__(self):
         self.df = pd.read_csv("../fileFromDb/inputFile.csv")
+        self.file_object = open("../TrainingLogs/preprocessingLogs.txt", "a+")
+        self.logger = Logger()
+
 
     def removeUnnecessaryFeatureColumn(self,column_name):
 
@@ -34,7 +38,12 @@ class PreprocessingMethods:
         :return: None
 
         """
-        self.df.drop(columns=[column_name], inplace=True)
+        try:
+            self.df.drop(columns=[column_name], inplace=True)
+            self.logger.log(self.file_object, f"Feature column named {column_name} removed from the dataframe")
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while removing a feature column {column_name} from the dataframe. Exception: {str(e)} ")
+
 
 
     def datatypeToDatetime(self,column_name):
@@ -52,7 +61,13 @@ class PreprocessingMethods:
         :return: None
 
         """
-        self.df[column_name] = pd.to_datetime(self.df[column_name], format="%d/%m/%Y")
+        try:
+            self.df[column_name] = pd.to_datetime(self.df[column_name], format="%d/%m/%Y")
+            self.logger.log(self.file_object, f"Datatype of feature column named {column_name} changed to datetime..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while changing the datatype of column {column_name} to datetime. Exception: {str(e)}")
+
 
 
     def splittingDatetimeColumnIntoThree(self,column_name):
@@ -72,10 +87,16 @@ class PreprocessingMethods:
 
         """
 
-        self.df['Day_of_Journey'] = self.df['Date_of_Journey'].dt.day
-        self.df['Month_of_Journey'] = self.df['Date_of_Journey'].dt.month
-        self.df['Year_of_Journey'] = self.df['Date_of_Journey'].dt.year
-        self.removeUnnecessaryFeatureColumn("Date_of_Journey")
+        try:
+            self.df['Day_of_Journey'] = self.df['Date_of_Journey'].dt.day
+            self.df['Month_of_Journey'] = self.df['Date_of_Journey'].dt.month
+            self.df['Year_of_Journey'] = self.df['Date_of_Journey'].dt.year
+            self.logger.log(self.file_object, "Successfully split the datetime column into three newly created columns.")
+            self.removeUnnecessaryFeatureColumn("Date_of_Journey")
+            self.logger.log(self.file_object, f"Original datetime column removed successfully..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while splitting the datetime column into three newly created columns. Exception: {str(e)}")
 
     def convertDurationIntoMinutes(self):
 
@@ -94,32 +115,43 @@ class PreprocessingMethods:
 
         """
 
-        pattern1 = re.compile(r"(\d+)(h|m)(\s)(\d*)(h|m)*")
-        pattern2 = re.compile(r"(\s*)(\d+)(h)")
-        pattern3 = re.compile(r"(\s*)(\d+)(m)")
+        try:
+            pattern1 = re.compile(r"(\d+)(h|m)(\s)(\d*)(h|m)*")
+            pattern2 = re.compile(r"(\s*)(\d+)(h)")
+            pattern3 = re.compile(r"(\s*)(\d+)(m)")
 
-        min_lst = []
-        for i in range(self.df.shape[0]):
-            if 'h' in self.df.loc[i, "Duration"] and 'm' in self.df.loc[i, "Duration"]:
-                matchobj = re.match(pattern1, self.df.loc[i, "Duration"])
-                hour = int(matchobj.group(1))
-                minute = int(matchobj.group(4))
-                total_min = 60 * hour + minute
-                min_lst.append(total_min)
-            elif 'h' in self.df.loc[i, "Duration"] and 'm' not in self.df.loc[i, "Duration"]:
-                matchobj = re.match(pattern2, self.df.loc[i, "Duration"])
-                hour = int(matchobj.group(2))
-                min_lst.append(60 * hour)
-            elif 'h' not in self.df.loc[i, "Duration"] and 'm' in self.df.loc[i, "Duration"]:
-                matchobj = re.match(pattern3, self.df.loc[i, "Duration"])
-                minute = int(matchobj.group(2))
-                min_lst.append(minute)
-            else:
-                min_lst.append(self.df.loc[i, "Duration"])
+            self.logger.log(self.file_object, "Created patterns which could match the values in the Duration column")
 
-        self.removeUnnecessaryFeatureColumn("Duration")
-        train_values = pd.Series(min_lst)
-        self.df.insert(loc=7, column="Flight_Duration", value=train_values)
+            min_lst = []
+            for i in range(self.df.shape[0]):
+                if 'h' in self.df.loc[i, "Duration"] and 'm' in self.df.loc[i, "Duration"]:
+                    matchobj = re.match(pattern1, self.df.loc[i, "Duration"])
+                    hour = int(matchobj.group(1))
+                    minute = int(matchobj.group(4))
+                    total_min = 60 * hour + minute
+                    min_lst.append(total_min)
+                elif 'h' in self.df.loc[i, "Duration"] and 'm' not in self.df.loc[i, "Duration"]:
+                    matchobj = re.match(pattern2, self.df.loc[i, "Duration"])
+                    hour = int(matchobj.group(2))
+                    min_lst.append(60 * hour)
+                elif 'h' not in self.df.loc[i, "Duration"] and 'm' in self.df.loc[i, "Duration"]:
+                    matchobj = re.match(pattern3, self.df.loc[i, "Duration"])
+                    minute = int(matchobj.group(2))
+                    min_lst.append(minute)
+                else:
+                    min_lst.append(self.df.loc[i, "Duration"])
+
+            self.logger.log(self.file_object, "Created a list which contains all the duration in minutes successfully..")
+
+            self.removeUnnecessaryFeatureColumn("Duration")
+            self.logger.log(self.file_object, "Removing the original Duratin column from the dataframe..")
+
+            train_values = pd.Series(min_lst)
+            self.df.insert(loc=7, column="Flight_Duration", value=train_values)
+            self.logger.log(self.file_object, "Added a new column named Flight_Duration which contains the flight duration in minutes..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while converting the flight duration into minutes. Exception: {str(e)}")
 
 
     def makeTotalStopsInteger(self):
@@ -137,9 +169,14 @@ class PreprocessingMethods:
         :return: None
         """
 
-        dict1 = {'non-stop': 0, '1 stop': 1, '2 stops': 2, '3 stops': 3, '4 stops': 4}
+        try:
+            dict1 = {'non-stop': 0, '1 stop': 1, '2 stops': 2, '3 stops': 3, '4 stops': 4}
 
-        self.df['Total_Stops'] = self.df['Total_Stops'].map(dict1)
+            self.df['Total_Stops'] = self.df['Total_Stops'].map(dict1)
+            self.logger.log(self.file_object, "Suceessfully changed the datatypes of the values from the Total_Stops columns..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while making values from column Total_Stops into integer. Exception: {str(e)}")
 
 
     def removingdDuplicateRows(self):
@@ -157,7 +194,11 @@ class PreprocessingMethods:
         
         """
         
-        self.df.drop_duplicates(inplace=True)
+        try:
+            self.df.drop_duplicates(inplace=True)
+            self.logger.log(self.file_object, "Removing of duplicate rows successful..")
+        except Exception as e:
+            self.logger.log(self.file_object,f"Exception occurred while removing duplicate rows from the dataframe. Exception: {str(e)}")
 
 
     def splittingTheDataframeIntoXandy(self):
@@ -174,10 +215,16 @@ class PreprocessingMethods:
         :return: X (dataframe with independent features) and y (dataframe with target feature)
         """
 
-        X = self.df.drop(columns=['Price'],axis=1)
-        y = self.df['Price']
+        try:
+            X = self.df.drop(columns=['Price'], axis=1)
+            y = self.df['Price']
+            self.logger.log(self.file_object, "Successfully split the dataframe into independent and dependent features..")
+            self.logger.log(self.file_object, "Returning X, y..")
+            return X, y
 
-        return X, y
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while splitting the dataframe into independent and dependent features. Exception: {str(e)}")
+
 
     # def findingNamesOfNumericalAndCategoricalColumns(self):
     #
@@ -218,8 +265,11 @@ class PreprocessingMethods:
 
         """
 
-        X, y = self.splittingTheDataframeIntoXandy()
-        X['Additional_Info'] = np.where(X['Additional_Info'] == "No Info", "No info", X['Additional_Info'])
+        try:
+            self.df['Additional_Info'] = np.where(self.df['Additional_Info'] == "No Info", "No info", self.df['Additional_Info'])
+            self.logger.log(self.file_object, "Successfully corrected the typos in the Additional_Info column..")
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while correcting the typo in the Additional_Info column. Exception: {str(e)}")
 
 
     def replacingOutliersWithNan(self):
@@ -236,14 +286,20 @@ class PreprocessingMethods:
         :return: None
         """
 
-        X, y = self.splittingTheDataframeIntoXandy()
+        try:
+            X, y = self.splittingTheDataframeIntoXandy()
 
-        for feature in X.columns:
-            Q1 = self.df[feature].quantile(0.25)
-            Q3 = self.df[feature].quantile(0.75)
-            IQR = Q3 - Q1
+            for feature in X.columns:
+                Q1 = self.df[feature].quantile(0.25)
+                Q3 = self.df[feature].quantile(0.75)
+                IQR = Q3 - Q1
 
-            self.df[feature] = np.where(self.df[feature] > (Q3 + 1.45 * IQR), np.nan, self.df[feature])
+                self.df[feature] = np.where((self.df[feature] > (Q3 + 1.45 * IQR) | self.df[feature] < (Q1 - 1.45 * IQR)), np.nan, self.df[feature])
+
+            self.logger.log(self.file_object, "Successfully replaced the outliers in the columns with null..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, F"Exception occurred while replacing outlier with the null values. Exception: {str(e)}")
 
     def fillingNullValues(self):
 
@@ -258,9 +314,48 @@ class PreprocessingMethods:
 
         :return: None
         """
-        X, y = self.splittingTheDataframeIntoXandy()
+        try:
+            X, y = self.splittingTheDataframeIntoXandy()
 
-        for feature in X.columns:
-            X[feature].fillna(X[feature].mean(),inplace=True)
+            for feature in X.columns:
+                self.df[feature].fillna(self.df[feature].mean(), inplace=True)
+
+            self.logger.log(self.file_object, "Successfully filled the null values from the columns with the mean of non-null values..")
+
+        except  Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while filling null values. Exception: {str(e)}")
+
+
+    def importProcessedFile(self):
+
+        """
+        Description: This method is used to export the processed file  as csv file
+
+        Written By: Shivam Shinde
+
+        Version: 1.0
+
+        Revision: None
+
+        :return: None
+        """
+
+        try:
+            self.df.to_csv('inputFileProcessed.csv',header=True,index=False)
+            self.logger.log(self.file_object, "Successfully exported the preprocessed input file..")
+
+        except Exception as e:
+            self.logger.log(self.file_object, f"Exception occurred while exporting the preprocessed input file. Exception: {str(e)}")
+
+
+
+
+
+
+
+
+
+
+
 
 
