31ec4e559ed809f4db5bc5d98339113a350c8cdd
Shivam Shinde <shivamshinde92722@gmail.com>
2022-03-12 02:46:28 +0530

    Created a python file named preprocessor.py which will implement the preprocessing of the data using methods created

---
+++ Preprocessing/preprocessor.py
@@ -0,0 +1,33 @@
+
+
+
+class Preprocessor:
+
+
+    """
+    Description: This class will contain a method which will implement all the preprocessing techniques on the date
+    to make it into clean data
+
+
+    Written By: Shivam Shinde
+
+    Version: 1.0
+
+    Revision: None
+    """
+
+
+
+    def preprocess(self):
+
+        """
+        Description: This method will implement all the preprocessing techniques on the date to make the data clean
+
+        Written By: Shivam Shinde
+
+        Version: 1.0
+
+        Revision: None
+        :return: None
+        """
+
