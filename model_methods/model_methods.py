66415f30d1f1d089d017f07301825c0401e4c90f
Shivam Shinde <shivamshinde92722@gmail.com>
2022-03-14 02:09:56 +0530

    Created a class and method for the model saving, model loading and model and finding correct model for the given cluster number

--- model_methods/model_methods.py
+++ model_methods/model_methods.py
@@ -7,7 +7,6 @@ from Logging.logging import Logger
 
 
 class modelMethods:
-
     """
 
     Description: This class will contain the methods used for saving, loading and finding the correct model for
@@ -23,11 +22,10 @@ class modelMethods:
 
     def __int__(self):
         self.logger = Logger()
-        self.file_object = open("../TrainingLogs/modelMethodsLogs.txt","a+")
+        self.file_object = open("../TrainingLogs/modelMethodsLogs.txt", "a+")
         self.model_directory = "../Models"
 
-
-    def modelSaving(self,model,filename):
+    def modelSaving(self, model, filename):
 
         """
         Description: This method is used to save the created model as a python pickle file
@@ -43,13 +41,15 @@ class modelMethods:
         try:
             path = os.path.join(self.model_directory, filename)
 
+            # removing the previously created models
             if os.path.exists(path):
                 shutil.rmtree(self.model_directory)
                 os.makedirs(path)
             else:
                 os.makedirs(path)
 
-            joblib.dump(model,os.path.join(path, f"{filename}.pkl"))
+            # saving the model as a python pickle file
+            joblib.dump(model, os.path.join(path, f"{filename}.pkl"))
 
             self.logger.log(self.file_object, f"Model {model} saved successfully in {path}")
 
@@ -57,8 +57,7 @@ class modelMethods:
             self.logger.log(self.file_object, f"Exception occurred while saving the model {model}. Exception: {str(e)}")
             raise e
 
-
-    def loadingSavedModel(self,filename):
+    def loadingSavedModel(self, filename):
 
         """
         Description: This method is used to load the saved method for the respective cluster
@@ -75,8 +74,12 @@ class modelMethods:
         try:
             self.logger.log(self.file_object, f"Loading the model {filename}.pkl")
             path = os.path.join(self.model_directory, filename, f"{filename}.pkl")
+
+            # loading the saved model
             model = joblib.load(path)
             self.logger.log(self.file_object, f"Model {filename} loaded successfully")
+
+            # returning the model
             return model
 
         except  Exception as e:
@@ -84,8 +87,7 @@ class modelMethods:
                                               f"Exception: {str(e)}")
             raise e
 
-
-    def findCorrectModel(self,clusterNumber):
+    def findCorrectModel(self, clusterNumber):
 
         """
         Description: This method is used to find the correct model given the  cluster number
@@ -101,7 +103,9 @@ class modelMethods:
         """
 
         self.logger.log(self.file_object, f"Finding the appropriate model for cluster number {clusterNumber}")
+        self.clusterNumber = clusterNumber
         try:
+            # finding the appropriate model for the given cluster number
             for file in os.listdir(self.model_directory):
 
                 if (file.index(str(clusterNumber))) == -1:
@@ -111,6 +115,8 @@ class modelMethods:
 
             self.logger.log(self.file_object, f"Successfully found the name of the model for the cluster number "
                                               f"{clusterNumber}")
+
+            # returning the model
             return self.model_name
 
         except Exception as e:
